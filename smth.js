7a6891262416b99765a5115637558279250d543f
hj <aspyinfinite@gmail.com>
2024-12-03 08:35:15 +0530

    js com

--- smth.js
+++ smth.js
@@ -1,1 +1,17 @@
-<script src="smooth-scroll.js"></script>
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+    const headerName = document.querySelector('header h1');
+    headerName.style.transition = 'all 0.5s ease-in-out';
+
+    headerName.addEventListener('mouseover', () => {
+        headerName.style.transform = 'scale(1.2)';
+        headerName.style.color = '#ffeb3b';
+    });
+
+    headerName.addEventListener('mouseout', () => {
+        headerName.style.transform = 'scale(1)';
+        headerName.style.color = '#fff';
+    });
+});
+</script>
+
